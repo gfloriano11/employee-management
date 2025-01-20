326590558dae00dc248e0f7b16705431e1a2e172
gfloriano11 <gflorianodev@gmail.com>
2025-01-19 21:44:20 -0300

    refactor: core calls router to verify url

--- src/app/core/Core.php
+++ src/app/core/Core.php
@@ -2,38 +2,14 @@
 
     class Core{
 
-        public function start_app($urlGet){
+        public function start_app($url){
 
-            // var_dump($urlGet);
+            // add "session_start()" after, it possibility to use '$_SESSION';
 
-            if(empty($urlGet)){
+            $uri_info = Router::router($url);
 
-                // make auth verification
-
-                $controller = 'HomeController';
-                $method = ' home';
-
-                echo $controller;
-                echo $method;
-                
-            } else {
-
-                // make if to home, and if to auth
-
-                $uri = explode('/', $urlGet['url']);
-
-                $page = $uri[0];
-
-                $controller = ucfirst($uri[0]) . 'Controller';
-                echo $controller;
-
-                if($page === 'admin'){
-                    if($uri[1] === 'funcionarios'){
-                        $method = $page;
-                        echo $method;
-                    } 
-                }
-            }
+            $controller = $uri_info['controller'];
+            $method = $uri_info['method'];
 
             call_user_func_array(array(new $controller, $method), array());
         }
