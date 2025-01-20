b7c5a92813d35a3c2f1eab45c3e1ceec092b3fa7
gfloriano11 <gflorianodev@gmail.com>
2025-01-20 20:43:51 -0300

    feat: create controller to auth methods

---
+++ src/app/controller/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+    class AuthController{
+
+        public function register(){
+            echo 'registre-se!';
+        }
+
+        public function login(){
+            echo 'fazer login!';
+        }
+    }
