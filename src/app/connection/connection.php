7fe56fbf3e82a6b2fa4b7fe0a0becbd942d25867
gfloriano11 <gflorianodev@gmail.com>
2025-01-26 14:43:58 -0300

    feat: create file to connect db and app

---
+++ src/app/connection/connection.php
@@ -0,0 +1,26 @@
+<?php
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'employee_management');
+
+    abstract class Connection{
+
+        private static $conn;
+
+        public static function getConn(){
+
+            if(self::$conn){
+                self::$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            }
+
+            return self::$conn;
+        }
+
+        public static function endConn(){
+            if(self::$conn){
+                self::$conn->close();
+            }
+        }
+    }
