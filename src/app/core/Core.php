997e1697434104ef91088de8d35fc30514ea4e17
gfloriano11 <gflorianodev@gmail.com>
2025-01-27 22:05:38 -0300

    feat: login working

--- src/app/core/Core.php
+++ src/app/core/Core.php
@@ -1,7 +1,11 @@
 <?php
 
     class Core{
-
+        
+        private $user;
+        public function __construct(){
+            $this->user = $_SESSION['user'] ?? null;
+        }
         public function start_app($url){
 
             // add "session_start()" after, it possibility to use '$_SESSION';
@@ -11,6 +15,27 @@
             $controller = $uri_info['controller'];
             $method = $uri_info['method'];
 
+            if($this->user){
+                $routes = ['HomeController/menu'];
+
+                if(isset($controller)){
+
+                    $controller = "HomeController";
+                    $method = 'menu';
+                } 
+            } else {
+                $routes = ['', 'HomeController/home', 'AuthController/login', 'AuthController/register'];
+
+                echo 'n tem user ';
+
+                if(isset($controller)){
+
+                    $controller = "HomeController";
+                    $method = 'home';
+                } 
+            }
+
             call_user_func_array(array(new $controller, $method), array());
         }
+
     }
