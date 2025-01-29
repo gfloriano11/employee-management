75f230420edd619ae3e78d37ed84e9182c35c723
gfloriano11 <gflorianodev@gmail.com>
2025-01-29 19:03:42 -0300

    feat: employee controller shows profile page

--- src/app/controller/EmployeeController.php
+++ src/app/controller/EmployeeController.php
@@ -3,6 +3,13 @@
     class EmployeeController{
 
         public function employee(){
-            echo 'oi';
+
+            $loader = new Twig\Loader\FilesystemLoader('../src/app/view/user');
+            $twig = new Twig\Environment($loader);
+            $template = $twig->load('profile.html');
+            
+            $content = $template->render();
+
+            echo $content;
         }
     }
