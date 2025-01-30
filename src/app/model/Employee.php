789abec433c92484765bab364424a1b7d2e934e9
gfloriano11 <gflorianodev@gmail.com>
2025-01-30 20:59:58 -0300

    feat: create function get info from user

---
+++ src/app/model/Employee.php
@@ -0,0 +1,27 @@
+<?php
+
+    class Employee{
+
+        public static function get_info($user_id){
+
+            $conn = Connection::getConn();
+            
+            $query = "SELECT * FROM users WHERE id = ?";
+
+            $statement = $conn->prepare($query);
+
+            $statement->bind_param('i', $user_id);
+
+            $statement->execute();
+
+            $result = $statement->get_result();
+
+            while($row = $result->fetch_assoc()){
+                $data = $row; 
+            }
+            
+            Connection::endConn();
+
+            return $data;
+        }
+    }
