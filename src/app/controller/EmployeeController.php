1cfa36e9663a7b955ce393d99f32ad3b6c6745ed
gfloriano11 <gflorianodev@gmail.com>
2025-01-30 21:00:27 -0300

    feat: employee controller use get info function

--- src/app/controller/EmployeeController.php
+++ src/app/controller/EmployeeController.php
@@ -4,11 +4,23 @@
 
         public function employee(){
 
+            $url = $_GET['url'];
+
+            $uri = explode('/', $url);
+
+            $id = $uri[1];
+
             $loader = new Twig\Loader\FilesystemLoader('../src/app/view/user');
             $twig = new Twig\Environment($loader);
             $template = $twig->load('profile.html');
+
+            $data['user'] = Employee::get_info($id);
+
+            // var_dump($data);
+
+            // echo $data['user']['id'];
             
-            $content = $template->render();
+            $content = $template->render($data);
 
             echo $content;
         }
