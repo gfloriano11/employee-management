69776c9340e3d732918c21bc15b943311b436d6c
gfloriano11 <gflorianodev@gmail.com>
2025-02-07 20:18:46 -0300

    fix: sql script

--- src/assets/database/employee-management.sql
+++ src/assets/database/employee-management.sql
@@ -6,19 +6,10 @@ USE employee_management;
 
 CREATE TABLE users(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     user_pass VARCHAR(255) NOT NULL,
     post VARCHAR(255) DEFAULT "Estagiário",
-    is_admin ENUM ("YES", "NO") DEFAULT "NO" NOT NULL
+    is_admin ENUM ("YES", "NO") DEFAULT "NO" NOT NULL,
+    profile_pic VARCHAR(255) DEFAULT "default.svg" NOT NULL
 );
-
-/* 
-
-OPTIONAL - INSERT ADMIN USER
-
-INSERT INTO users
-(first_name, surname, email, user_pass, post, is_admin)
-VALUES
-('The', 'Admin', 'admin@admin.com', 'password', 'admintrator', 'YES');
